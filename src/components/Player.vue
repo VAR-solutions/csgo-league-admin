de02cbc3ac50f707fe1118f911f9644f0d6e00f1
Ravi Varshney <ravivarshney49@gmail.com>
2019-08-16 22:40:01 +0530

    Fixed UI

--- src/components/Player.vue
+++ src/components/Player.vue
@@ -1,10 +1,10 @@
 <template>
   <v-flex xs12 sm6 md3>
-    <v-card class="mx-auto" tile :to="'/players/'+data.uid">
+    <v-card class="mx-auto my-2" width="95%" tile :to="'/players/'+data.uid">
       <v-layout align-center justify-center pt-4>
-        <v-avatar size="85%">
+        <v-avatar size="75%">
           <img
-            src="https://microhealth.com/assets/images/illustrations/personal-user-illustration-@2x.png"
+            src="https://firebasestorage.googleapis.com/v0/b/csgo-auction.appspot.com/o/side-03.png?alt=media&token=91ce7293-e061-4a46-8929-d72bb6917499"
           />
         </v-avatar>
       </v-layout>

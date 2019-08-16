f8db143f0cd3675dc6b4cfeed53d4b0046d6b224
Ravi Varshney <ravivarshney49@gmail.com>
2019-08-17 00:31:52 +0530

    added side- navigation

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,35 @@
 <template>
   <v-app>
-      <v-app-bar absolute app dark flat>
-        <v-toolbar-title class="headline text-uppercase">
-          <span>C S : G O</span>
-          <span class="font-weight-light"> League</span>
-        </v-toolbar-title>
-        <v-spacer></v-spacer>
-        <v-toolbar-items color="#303030" exact>
-          <v-btn text :to="{name: 'Home'}" exact>Home</v-btn>
-          <v-btn text :to="{ name: 'Players'}" exact>Players</v-btn>
-          <v-btn text exact>Teams</v-btn>
-          <v-btn text exact>Auction</v-btn>
-          <v-btn text :to="{ name: 'Announcements' }" exact>Announcements</v-btn>
-          <v-btn text exact>Fixtures</v-btn>
-          <v-btn text exact>Points Table</v-btn>
-          <v-btn text exact v-if="currentUser" @click="logout">LOGOUT</v-btn>
-        </v-toolbar-items>
-      </v-app-bar>
+    <v-app-bar absolute app dark flat>
+      <v-app-bar-nav-icon class="hidden-md-and-up" @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title class="headline text-uppercase">
+        <span>C S : G O</span>
+        <span class="font-weight-light">League</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items class="hidden-sm-and-down" color="#303030" exact>
+        <v-btn text :to="{name: 'Home'}" exact>Home</v-btn>
+        <v-btn text :to="{ name: 'Players'}" exact>Players</v-btn>
+        <v-btn text exact>Teams</v-btn>
+        <v-btn text exact>Auction</v-btn>
+        <v-btn text :to="{ name: 'Announcements' }" exact>Announcements</v-btn>
+        <v-btn text exact>Fixtures</v-btn>
+        <v-btn text exact>Points Table</v-btn>
+        <v-btn text exact v-if="currentUser" @click="logout">LOGOUT</v-btn>
+      </v-toolbar-items>
+    </v-app-bar>
+    <v-navigation-drawer dark v-model="drawer" absolute temporary>
+      <v-list nav dense>
+          <v-list-item active-class="grey--text text--accent-4" :to="{name: 'Home'}" exact>Home</v-list-item>
+          <v-list-item active-class="grey--text text--accent-4" :to="{ name: 'Players'}" exact>Players</v-list-item>
+          <v-list-item active-class="grey--text text--accent-4" exact>Teams</v-list-item>
+          <v-list-item active-class="grey--text text--accent-4" exact>Auction</v-list-item>
+          <v-list-item active-class="grey--text text--accent-4" :to="{ name: 'Announcements' }" exact>Announcements</v-list-item>
+          <v-list-item active-class="grey--text text--accent-4" exact>Fixtures</v-list-item>
+          <v-list-item active-class="grey--text text--accent-4" exact>Points Table</v-list-item>
+          <v-list-item active-class="grey--text text--accent-4" exact v-if="currentUser" @click="logout">LOGOUT</v-list-item>
+      </v-list>
+    </v-navigation-drawer>
     <v-content>
       <router-view />
     </v-content>
@@ -38,7 +51,9 @@ const fb = require("./firebaseConfig");
 import { mapState } from "vuex";
 export default {
   name: "App",
-  data: () => ({}),
+  data: () => ({
+    drawer: null
+  }),
   computed: {
     ...mapState(["currentUser"])
   },

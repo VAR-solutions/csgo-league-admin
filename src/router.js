899708fd3c4f1d0c4807e727b82c2fa2c56a04e2
Ravi Varshney <ravivarshney49@gmail.com>
2019-08-19 00:29:51 +0530

    category and auction completed

--- src/router.js
+++ src/router.js
@@ -3,14 +3,14 @@ import Router from "vue-router";
 import Home from "./views/Home.vue";
 import Announcements from "./views/Announcements.vue";
 import Login from "./components/Login.vue";
-import firebase from 'firebase/app';
-import Players from './views/Players';
-import Profile from './views/Profile';
-import Auction from './views/Auction';
-import Teams from './views/Teams';
-import Fixtures from './views/Fixtures';
-import PointsTable from './views/PointsTable'
-import 'firebase/auth'
+import firebase from "firebase/app";
+import Players from "./views/Players";
+import Profile from "./views/Profile";
+import Auction from "./views/Auction";
+import Teams from "./views/Teams";
+import Fixtures from "./views/Fixtures";
+import PointsTable from "./views/PointsTable";
+import "firebase/auth";
 
 Vue.use(Router);
 
@@ -18,24 +18,24 @@ const router = new Router({
   mode: "history",
   routes: [
     {
-      path: '*',
-      redirect: '/'
+      path: "*",
+      redirect: "/"
     },
     {
-      path: '/login',
-      name: 'Login',
+      path: "/login",
+      name: "Login",
       component: Login,
       meta: {
-        title: 'Login to ADMIN | CS:GO League - Arcadia'
+        title: "Login to ADMIN | CS:GO League - Arcadia"
       }
     },
     {
-      path: '/',
-      name: 'Home',
+      path: "/",
+      name: "Home",
       component: Home,
       meta: {
         requiresAuth: true,
-        title: 'Home Page | CS:GO League - Arcadia | ADMIN'
+        title: "Home Page | CS:GO League - Arcadia | ADMIN"
       }
     },
     {
@@ -53,7 +53,7 @@ const router = new Router({
       component: Announcements,
       meta: {
         requiresAuth: true,
-        title: 'Announcements | CS:GO League - Arcadia | ADMIN'
+        title: "Announcements | CS:GO League - Arcadia | ADMIN"
       }
     },
     {
@@ -62,7 +62,7 @@ const router = new Router({
       component: Players,
       meta: {
         requiresAuth: true,
-        title: 'Players | CS:GO League - Arcadia | ADMIN'
+        title: "Players | CS:GO League - Arcadia | ADMIN"
       }
     },
     {
@@ -72,7 +72,7 @@ const router = new Router({
       props: true,
       meta: {
         requiresAuth: true,
-        title: 'Player\'s Profile | CS:GO League - Arcadia | ADMIN'
+        title: "Player's Profile | CS:GO League - Arcadia | ADMIN"
       }
     },
     {
@@ -81,7 +81,7 @@ const router = new Router({
       component: Auction,
       meta: {
         requiresAuth: true,
-        title: 'Auction | CS:GO League - Arcadia | ADMIN'
+        title: "Auction | CS:GO League - Arcadia | ADMIN"
       }
     },
     {
@@ -90,7 +90,7 @@ const router = new Router({
       component: Teams,
       meta: {
         requiresAuth: true,
-        title: 'Teams | CS:GO League - Arcadia | ADMIN'
+        title: "Teams | CS:GO League - Arcadia | ADMIN"
       }
     },
     {
@@ -99,7 +99,7 @@ const router = new Router({
       component: Fixtures,
       meta: {
         requiresAuth: true,
-        title: 'Fixtures | CS:GO League - Arcadia | ADMIN'
+        title: "Fixtures | CS:GO League - Arcadia | ADMIN"
       }
     },
     {
@@ -108,7 +108,7 @@ const router = new Router({
       component: PointsTable,
       meta: {
         requiresAuth: true,
-        title: 'Points Table | CS:GO League - Arcadia | ADMIN'
+        title: "Points Table | CS:GO League - Arcadia | ADMIN"
       }
     }
   ]
@@ -117,30 +117,41 @@ const router = new Router({
 router.beforeEach((to, from, next) => {
   // This goes through the matched routes from last to first, finding the closest route with a title.
   // eg. if we have /some/deep/nested/route and /some, /deep, and /nested have titles, nested's will be chosen.
-  const nearestWithTitle = to.matched.slice().reverse().find(r => r.meta && r.meta.title);
+  const nearestWithTitle = to.matched
+    .slice()
+    .reverse()
+    .find(r => r.meta && r.meta.title);
 
   // Find the nearest route element with meta tags.
-  const nearestWithMeta = to.matched.slice().reverse().find(r => r.meta && r.meta.metaTags);
-  const previousNearestWithMeta = from.matched.slice().reverse().find(r => r.meta && r.meta.metaTags);
+  const nearestWithMeta = to.matched
+    .slice()
+    .reverse()
+    .find(r => r.meta && r.meta.metaTags);
+  const previousNearestWithMeta = from.matched
+    .slice()
+    .reverse()
+    .find(r => r.meta && r.meta.metaTags);
 
   // If a route with a title was found, set the document (page) title to that value.
   if (nearestWithTitle) document.title = nearestWithTitle.meta.title;
 
   // Remove any stale meta tags from the document using the key attribute we set below.
-  Array.from(document.querySelectorAll('[data-vue-router-controlled]')).map(el => el.parentNode.removeChild(el));
+  Array.from(document.querySelectorAll("[data-vue-router-controlled]")).map(
+    el => el.parentNode.removeChild(el)
+  );
 
   // Skip rendering meta tags if there are none.
   // if(!nearestWithMeta) return next();
-  next()
-  const requiresAuth = to.matched.some(x => x.meta.requiresAuth)
-  const currentUser = firebase.auth().currentUser
+  next();
+  const requiresAuth = to.matched.some(x => x.meta.requiresAuth);
+  const currentUser = firebase.auth().currentUser;
 
   if (requiresAuth && !currentUser) {
-    next('/login')
+    next("/login");
   } else if (!requiresAuth && currentUser) {
-    next('/')
+    next("/");
   } else {
-    next()
+    next();
   }
 });
 
--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -3,7 +3,7 @@
     <vue-element-loading
       is-full-screen
       :active="showCustomizeLoader"
-      background-color="rgba(100, 100, 100, 0.4)"
+      background-color="rgba(100, 100, 100, 1)"
     >
       <img
         src="https://firebasestorage.googleapis.com/v0/b/csgo-auction.appspot.com/o/side-03.png?alt=media&token=91ce7293-e061-4a46-8929-d72bb6917499"
@@ -19,24 +19,85 @@
             src="https://firebasestorage.googleapis.com/v0/b/csgo-auction.appspot.com/o/side-03.png?alt=media&token=91ce7293-e061-4a46-8929-d72bb6917499"
           />
         </v-col>
-        <v-col cols="12" md="7" xs="12" justify="center" align="center" class="mt-5">
-          <h1 class="fin">{{ data.tag}}</h1>
+        <v-col
+          cols="12"
+          md="7"
+          xs="12"
+          justify="center"
+          align="center"
+          class="mt-5"
+        >
+          <h1 class="fin">{{ data.tag }}</h1>
           <v-card-text>
-            <p class="lexend" style="font-size: 25px">{{ data.name}}</p>
-            <p class="size lexend">Primary Weapon: {{ data.primary_weapon}}</p>
-            <p class="size lexend">Secondary Weapon: {{ data.secondary_weapon}}</p>
-            <p class="size lexend">Category: To be decided</p>
-            <p class="size lexend">Team: To be Auctioned</p>
-            <h2>{{ data.category}}</h2>
+            <p class="lexend" style="font-size: 25px">{{ data.name }}</p>
+            <p class="size lexend">Primary Weapon: {{ data.primary_weapon }}</p>
+            <p class="size lexend">
+              Secondary Weapon: {{ data.secondary_weapon }}
+            </p>
+            <p class="size lexend" v-if="data.category">
+              Category: {{ data.category }}
+            </p>
+            <p class="size lexend" v-if="data.team">Team: {{ data.team }}</p>
           </v-card-text>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn @click="dialog = true">Assign Category</v-btn>
+            <v-btn @click="dialog2 = true">Add to Team</v-btn>
+          </v-card-actions>
         </v-col>
       </v-row>
     </v-card>
+    <v-dialog v-model="dialog" persistent max-width="600px">
+      <v-card>
+        <v-card-title>
+          <span class="headline align-center">Player Category</span>
+        </v-card-title>
+        <v-card-text>
+          <v-container>
+            <v-select
+              v-model="k"
+              :items="items"
+              dark
+              label="Category"
+            ></v-select>
+          </v-container>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn text @click="dialog = false">Close</v-btn>
+          <v-btn text @click="saveCategory">Save</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+    <v-dialog v-model="dialog2" persistent max-width="600px">
+      <v-card>
+        <v-card-title>
+          <span class="headline align-center">Auction</span>
+        </v-card-title>
+        <v-card-text>
+          <v-container>
+            <v-text-field label="Credit" v-model="credit"></v-text-field>
+            <v-select
+              v-model="team"
+              :items="team_names"
+              dark
+              label="Team"
+            ></v-select>
+          </v-container>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn text @click="dialog2 = false">Close</v-btn>
+          <v-btn text @click="save">Save</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
   </v-container>
 </template>
 <script>
 const fb = require("../firebaseConfig");
 import VueElementLoading from "vue-element-loading";
+import { functions } from "firebase";
 
 export default {
   name: "profile",
@@ -44,12 +105,21 @@ export default {
   data() {
     return {
       data: {},
-      showCustomizeLoader: true
+      showCustomizeLoader: true,
+      dialog: false,
+      dialog2: false,
+      items: ["Gold", "Silver", "Bronze"],
+      k: "",
+      team: "",
+      credit: null,
+      teams: [],
+      team_names: []
     };
   },
   components: {
     VueElementLoading
   },
+
   created() {
     setTimeout(() => {
       this.showCustomizeLoader = false;
@@ -60,6 +130,137 @@ export default {
       .then(res => {
         this.data = res.data();
       });
+    fb.teamsCollection.get().then(res => {
+      res.forEach(doc => {
+        // console.log(doc.data())
+        this.teams.push(doc.data());
+      });
+      this.teams.forEach(element => {
+        this.team_names.push(element.name);
+      });
+    });
+  },
+  methods: {
+    saveCategory() {
+      var predata = [];
+      var d = [];
+      var dd = [];
+      var id = this.$route.params.id;
+      var cat = this.data.category;
+      var new_cat = this.k;
+      this.showCustomizeLoader = true;
+      if (this.data.category == "") {
+        fb.auctionCollection
+          .doc(this.k.toLowerCase())
+          .get()
+          .then(res => {
+            // console.log(res.data().players)
+            predata = res.data().players;
+            predata.push(parseInt(this.$route.params.id));
+            fb.auctionCollection.doc(this.k.toLowerCase()).set({
+              players: predata
+            });
+            this.data.category = this.k;
+            fb.playersCollection.doc(this.$route.params.id).update({
+              category: this.k
+            });
+            this.showCustomizeLoader = false;
+            this.dialog = false;
+            this.data.category = this.k;
+          });
+      } else {
+        fb.auctionCollection
+          .doc(this.data.category.toLowerCase())
+          .get()
+          .then(res => {
+            d = res.data().players;
+            dd = d.filter(function(player) {
+              return player != parseInt(id);
+            });
+            fb.auctionCollection.doc(this.data.category.toLowerCase()).set({
+              players: dd
+            });
+            fb.auctionCollection
+              .doc(this.k.toLowerCase())
+              .get()
+              .then(r => {
+                predata = r.data().players;
+                predata.push(parseInt(id));
+                fb.auctionCollection.doc(this.k.toLowerCase()).set({
+                  players: predata
+                });
+                this.showCustomizeLoader = false;
+                this.dialog = false;
+                this.data.category = this.k;
+              });
+            fb.playersCollection.doc(id).update({
+              category: this.k
+            });
+          });
+      }
+    },
+    save() {
+      if (this.data.category && !this.data.team) {
+        let announcement = {};
+        announcement["announcement"] =
+          this.data.name +
+          " (" +
+          this.data.tag +
+          ") " +
+          " bought by " +
+          this.team +
+          " for " +
+          this.credit +
+          " credits";
+        announcement["time"] = Date.now();
+        var d = [];
+        var dd = [];
+        this.showCustomizeLoader = true;
+        var id = this.$route.params.id;
+        var tid;
+        this.teams.forEach(ele => {
+          if (ele.name === this.team) {
+            tid = ele.tid;
+          }
+        });
+        var ids = [];
+        fb.auctionCollection
+          .doc(this.data.category.toLowerCase())
+          .get()
+          .then(res => {
+            d = res.data().players;
+            dd = d.filter(function(player) {
+              return player != parseInt(id);
+            });
+            fb.auctionCollection.doc(this.data.category.toLowerCase()).set({
+              players: dd
+            });
+            this.showCustomizeLoader = false;
+            this.dialog2 = false;
+            this.$router.push({ name: "Auction" });
+          })
+          .catch(err => {
+            console.log(err);
+          });
+
+        fb.teamsCollection
+          .doc(tid.toString())
+          .get()
+          .then(res => {
+            ids = res.data().player_ids;
+            ids.push(id);
+            fb.teamsCollection.doc(tid).update({
+              player_ids: ids
+            });
+          });
+        fb.playersCollection.doc(id).update({
+          team: this.team
+        });
+        fb.announcementsCollection.add(announcement);
+      } else {
+        this.dialog2 = false;
+      }
+    }
   }
 };
 </script>

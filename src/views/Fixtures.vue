fd1dbe15fe5f7c4ffa975689942b4d47ac440d83
i-vishi <rr.vishalgaur@gmail.com>
2019-09-18 00:53:02 +0530

    added fixtures and points table

--- src/views/Fixtures.vue
+++ src/views/Fixtures.vue
@@ -1,14 +1,43 @@
 <template>
-  <v-container>
+  <!-- <v-container>
     <h1 class="fin" align="center">COMING SOON...</h1>
+  </v-container> -->
+
+  
+  <v-container>
+    <v-timeline align-top :dense="$vuetify.breakpoint.smAndDown">
+      <v-timeline-item v-for="(item, i) in fixtures" :key="i" color="#91ff00" fill-dot>
+        <v-card dark>
+          <v-card-title class="title fin">Match #{{ item.mnumber }}</v-card-title>
+          <v-card-text class="lexend">
+            <p>Date: {{ item.date }}</p>
+            <p style="font-size: 20px">{{item.team1}} vs {{item.team2}}</p>
+          </v-card-text>
+        </v-card>
+      </v-timeline-item>
+    </v-timeline>
   </v-container>
 </template>
 
 <script>
 import { mapState } from "vuex";
+const fb = require("../firebaseConfig");
 export default {
+  name: "fixtures",
+  data() {
+    return {
+      fixtures: []
+    };
+  },
   computed: {
     ...mapState(["currentUser"])
+  },
+  created() {
+    fb.fixturesCollection.orderBy("mnumber").get().then(res => {
+      res.forEach(doc => {
+        this.fixtures.push(doc.data());
+      })
+    })
   }
 };
 </script>
--- src/views/PointsTable.vue
+++ src/views/PointsTable.vue
@@ -1,9 +1,66 @@
 <template>
   <v-container>
-    <h1 class="fin" align="center">COMING SOON...</h1>
+    <!-- <h1 class="fin" align="center">COMING SOON...</h1> -->
+    <v-data-table
+      :headers="headers"
+      :items="teamstats"
+      item-key="name"
+      hide-default-footer
+      :loading="dataload"
+      loading-text="Loading... Please wait"
+      class="elevation-1"
+      sort-by="points"
+      sort-desc
+    ></v-data-table>
   </v-container>
 </template>
 
 <script>
-export default {};
+import { mapState } from "vuex";
+const fb = require("../firebaseConfig");
+export default {
+  name: "ptable",
+  data() {
+    return {
+      headers: [
+        {
+          text: "Team Name",
+          align: "left",
+          value: "name",
+          sortable: false
+        },
+        { text: "Matches Played", value: "mplayed", sortable: false},
+        { text: "Matches Won", value: "mwon", sortable: false },
+        { text: "Rounds Won", value: "rwon", sortable: false },
+        { text: "Rounds Lost", value: "rlost", sortable: false },
+        { text: "Round Difference", value: "rd", sortable: false },
+        { text: "Points", value: "points" }
+      ],
+      teamstats: [],
+      dataload: true,
+    };
+  },
+  computed: {
+    ...mapState(["currentUser"])
+  },
+  created() {
+    fb.teamsCollection.onSnapshot(snap => {
+      this.dataload = true;
+      this.teamstats = [];
+      snap.forEach(res => {
+        var teamdetails = {};
+        teamdetails["name"] = res.data().name
+        teamdetails["mplayed"] = res.data().mplayed
+        teamdetails["mwon"] = res.data().mwon
+        teamdetails["rwon"] = res.data().rwon
+        teamdetails["rlost"] = res.data().rlost
+        teamdetails["rd"] = teamdetails["rwon"] - teamdetails["rlost"]
+        var point = (teamdetails["mwon"] * 100) + teamdetails["rd"]
+        teamdetails["points"] = point
+        this.teamstats.push(teamdetails)
+      });
+      this.dataload = false;
+    })
+  }
+};
 </script>
